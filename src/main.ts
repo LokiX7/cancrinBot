9be89400d2e32e29f6fc5dcca9cf2cefcff1e797
Alexander Revenko <revenko.a@bk.ru>
2023-06-05 00:33:23 +0700

    move winstonOptions from main.ts to winston-options.ts

--- src/main.ts
+++ src/main.ts
@@ -1,45 +1,11 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { WinstonModule } from 'nest-winston';
 import { CbrExchangeModule } from './cbr-exchange/cbr-exchange.module';
 import { CbrExchangeService } from './cbr-exchange/cbr-exchange.service';
-import {
-  utilities as nestWinstonModuleUtilities,
-  WinstonModule,
-} from 'nest-winston';
-import * as winston from 'winston';
+import { winstonOptions } from './winston-options';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const winstonOptions = {
-    transports: [
-      new winston.transports.Console({
-        level: 'debug',
-        format: winston.format.combine(
-          winston.format.colorize(),
-          winston.format.timestamp(),
-          winston.format.simple(),
-        ),
-      }),
-      new winston.transports.File({
-        filename: 'error.log',
-        dirname: 'logs',
-        level: 'error',
-        format: winston.format.json(),
-      }),
-      new winston.transports.File({
-        filename: 'info.log',
-        dirname: 'logs',
-        level: 'info',
-        format: winston.format.json(),
-      }),
-      new winston.transports.File({
-        filename: 'combined.log',
-        dirname: 'logs',
-        level: 'debug',
-        format: winston.format.json(),
-      }),
-    ],
-  };
-
   const app = await NestFactory.create(AppModule, {
     logger: WinstonModule.createLogger(winstonOptions),
   });
---
+++ src/winston-options.ts
@@ -0,0 +1,32 @@
+import * as winston from 'winston';
+
+export const winstonOptions = {
+  transports: [
+    new winston.transports.Console({
+      level: 'debug',
+      format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.timestamp(),
+        winston.format.simple(),
+      ),
+    }),
+    new winston.transports.File({
+      filename: 'error.log',
+      dirname: 'logs',
+      level: 'error',
+      format: winston.format.json(),
+    }),
+    new winston.transports.File({
+      filename: 'info.log',
+      dirname: 'logs',
+      level: 'info',
+      format: winston.format.json(),
+    }),
+    new winston.transports.File({
+      filename: 'combined.log',
+      dirname: 'logs',
+      level: 'debug',
+      format: winston.format.json(),
+    }),
+  ],
+};
